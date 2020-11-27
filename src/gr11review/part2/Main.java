7aa8c8a8c3dbd1fa14f70c143e85ed4efde9cee6
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-11-27 15:03:21 +0000

    Switched Utility and Main. Mistake

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,8 @@
 package gr11review.part2;
 
+import gr11review.part2.Utility.*;
+public class Main {
+  public static void main(String[] args){
+
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,29 @@
 package gr11review.part2;
+public class Utility{
+  public static int sumNumbers(String str){
+    int intLength;
+    intLength = str.Length();
+    int intCount;
+    int intSum = 0;
+    String strWord = "";
+    for(intCount = 0; intCount < intLength; intCount++){
+      if(Character.isDigit(strcharAt(intCount))) {
+        if (intCount < intLength-1 && Character.isDigit(strCharAt(intCount+1))){
+          strWord += strCharAt(intCount);
+        }else{
+          strWord += strCharAt(intCount);
+          intSum += Integer.parseInt(strWord);
+          strWord = "";
+        }
+      }
+    }
+    return intSum;
+  }
+}
 
+import gr11review.part2.Utility.*;
 public class Utility {
-    
+  public static void main(String[] args){
+
+  }
 }
